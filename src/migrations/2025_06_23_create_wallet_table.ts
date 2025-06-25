3fc6c98ed4cd53d033f544f6f89ce573d27e1660
Dave <uchennadavid2404@gmail.com>
2025-06-25 17:16:46 +0100

    fix: make walletAddress field not nullable and unique in create_wallet_table migration

--- src/migrations/2025_06_23_create_wallet_table.ts
+++ src/migrations/2025_06_23_create_wallet_table.ts
@@ -5,7 +5,7 @@ export async function up(knex: Knex): Promise<void> {
 		table.uuid('id').primary().defaultTo(knex.raw('(UUID())'));
 		table.uuid('userId').references('id').inTable('users').onDelete('RESTRICT');
 		table.integer('balance').notNullable().defaultTo(0);
-        table.uuid('walletAddress').defaultTo(knex.raw('(UUID())'));
+		table.uuid('walletAddress').notNullable().unique().defaultTo(knex.raw('(UUID())'));
 		table.boolean('isDeleted').notNullable().defaultTo(false);
 		table.timestamps(true, true);
 		table.unique(['userId']);
